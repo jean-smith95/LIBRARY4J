6a952f9ce44709600afa86388ba54743df551f8b
jean-smith95 <101268854+jean-smith95@users.noreply.github.com>
2022-04-13 17:20:00 +0200

    Add files via upload

--- Connexion.java
+++ Connexion.java
@@ -2,13 +2,14 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class Connexion {
+    //method to connect to the Database
     public static Connection connectorDB() {
         try {
             Class.forName("com.mysql.jdbc.Driver");
             System.out.println("Driver oki");
             String url = "jdbc:mysql://localhost:3306/library";
             String user = "root";
-            String password = "SMith01022001**";
+            String password = "Zoo01022001**";
             Connection cnx = DriverManager.getConnection(url, user, password);
             System.out.println("Connexion bien établie ");
 
--- MainBdd.java
+++ MainBdd.java
@@ -1,17 +1,29 @@
 import java.sql.*;
+import java.util.Scanner;
+
 public class MainBdd {
 
     public static void main(String[] args) throws SQLException {
-      //  Connection cnt = Connexion.connectorDB();
-       // Statement st = cnt.createStatement();
-        //ResultSet res = st.executeQuery("SELECT * FROM books");
-        Management.registrer("spe","test4");
-       // while(res.next()){
-          //  System.out.println("Livre :\nId :"+res.getString("idBook")+
-          //          "\nAuteur : "+res.getString("author")+
-         //           "\nTitre : "+res.getString("Title"));
-       // }
 
+        Connection cnt = Connexion.connectorDB();
+        Statement st = cnt.createStatement();
+        //Execute th
+        ResultSet res = st.executeQuery("SELECT * FROM books");
+        //Saisie d'un pseudo et mdp
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Saisissez un pseudo : ");
+        String pseudo = sc.nextLine();
+        System.out.println("Saisissez un mot de passe : ");
+        String password = sc.nextLine();
+        System.out.println("FIN ! ");
+      // Management.registrer(pseudo,password);
+        Management.connection(pseudo,password);
+        while(res.next()){
+           System.out.println("Livre :\nId :"+res.getString("idBook")+
+                   "\nAuteur : "+res.getString("author")+
+                  "\nTitre : "+res.getString("Title"));
+       }
+      //  Management.loanBooktoUserDB("js",2);
     }
 
 
