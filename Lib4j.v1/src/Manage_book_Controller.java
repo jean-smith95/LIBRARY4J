b695d3746e842c491c628046234985eaf58e5a9f
Bruneck Agomadje <60816549+iamaub@users.noreply.github.com>
2022-04-20 21:48:16 +0200

    Add files via upload

---
+++ Lib4j.v1/src/Manage_book_Controller.java
@@ -0,0 +1,33 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+
+public class Manage_book_Controller {
+    public Manage_book_Controller()
+    {
+
+    }
+
+    public void HomeClicked(ActionEvent event) {
+    }
+
+    public void ReturnBookClicked(ActionEvent event) {
+    }
+
+    public void AssignBookClicked(ActionEvent event) {
+    }
+
+    public void ListBooksClicked(ActionEvent event) {
+    }
+
+    public void IssueBookClicked(ActionEvent event) {
+    }
+
+    public void AddBookClicked(ActionEvent event) {
+    }
+
+    public void LogoutClicked(ActionEvent event) {
+    }
+}
---
+++ Lib4j.v1/src/Splash_Controller.java
@@ -0,0 +1,15 @@
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Splash_Controller implements Initializable {
+   @FXML
+    private AnchorPane rootPane;
+   @Override
+    public void initialize(URL url, ResourceBundle rb){}
+
+}
+
